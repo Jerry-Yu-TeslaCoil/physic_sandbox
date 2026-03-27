1243ed30f54c400785716a27a97e8a24c460d341
Jerry-Yu-TeslaCoil <jerry.050222@outlook.com>
2026-03-27 20:48:25 +0800

    feat: 映射器从映射事件类型改为映射事件抽象工厂

---
+++ src/main/java/com/game/physicsandbox/physics/control/ControlEventFactory.java
@@ -0,0 +1,30 @@
+package com.game.physicsandbox.physics.control;
+
+
+import com.game.physicsandbox.physics.event.Event;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * 控制事件工厂。用于控制器根据用户输入生成事件。
+ * 分为键盘事件和鼠标事件。
+ *
+ * @author Jerry-Yu-TeslaCoil
+ * @version 1.0
+ */
+public abstract class ControlEventFactory {
+
+    /**
+     * 根据键盘事件创建控制事件
+     * @param event 输入的键盘事件
+     * @return 创建的控制事件
+     */
+    public abstract Event create(KeyEvent event);
+
+    /**
+     * 根据鼠标事件创建控制事件
+     * @param event 输入的鼠标事件
+     * @return 创建的控制事件
+     */
+    public abstract Event create(MouseEvent event);
+}
--- src/main/java/com/game/physicsandbox/physics/control/InputMapper.java
+++ src/main/java/com/game/physicsandbox/physics/control/InputMapper.java
@@ -1,6 +1,5 @@
 package com.game.physicsandbox.physics.control;
 
-import com.game.physicsandbox.physics.event.Event;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
@@ -11,7 +10,7 @@ import java.util.Map;
 
 /**
  * 按键映射器，用于将用户输入映射为事件
- * 映射器应根据给出的按键，返回映射到的事件类型，供Controller创建新事件并配置事件信息
+ * 映射器应根据给出的按键，返回映射到的事件类型工厂，供Controller创建新事件并配置事件信息
  * 同时，映射器应管理相同类型按键互斥
  * 分为鼠标事件、键盘事件
  *
@@ -20,24 +19,55 @@ import java.util.Map;
  */
 public class InputMapper {
 
-    Map<MouseButton, Class<? extends Event>> mouseMapping = new HashMap<>();
-    Map<Class<? extends Event>, MouseButton> invertMouseMapping = new HashMap<>();
+    Map<MouseButton, ControlEventFactory> mouseMapping = new HashMap<>();
+    Map<ControlEventFactory, MouseButton> invertMouseMapping = new HashMap<>();
 
-    Map<KeyCode, Class<? extends Event>> keyMapping = new HashMap<>();
-    Map<Class<? extends Event>, KeyCode> invertKeyMapping = new HashMap<>();
+    Map<KeyCode, ControlEventFactory> keyMapping = new HashMap<>();
+    Map<ControlEventFactory, KeyCode> invertKeyMapping = new HashMap<>();
 
-    public void setMapping(MouseButton button, Class<? extends Event> event) {
+    /**
+     * 设置鼠标按钮映射关系。新的关系会置空原先冲突的映射
+     * @param button 鼠标按钮
+     * @param factory 对应事件工厂
+     */
+    public void setMapping(MouseButton button, ControlEventFactory factory) {
         MouseButton conflictedButton;
-        if ((conflictedButton = invertMouseMapping.getOrDefault(event, null)) != null) {
-            //TODO: 处理键位冲突
+        if ((conflictedButton = invertMouseMapping.getOrDefault(factory, null)) != null) {
+            mouseMapping.put(conflictedButton, null);
         }
+        mouseMapping.put(button, factory);
+        invertMouseMapping.put(factory, button);
     }
 
-    public Class<? extends Event> getMappingEvent(MouseEvent event) {
-        return mouseMapping.getOrDefault(event.getButton(), Event.class);
+    /**
+     * 设置映射关系。新的关系会置空原先冲突的映射
+     * @param keyCode 键盘按钮码
+     * @param factory 对应事件工厂
+     */
+    public void setMapping(KeyCode keyCode, ControlEventFactory factory) {
+        KeyCode conflictedKeyCode;
+        if ((conflictedKeyCode = invertKeyMapping.getOrDefault(factory, null)) != null) {
+            keyMapping.put(conflictedKeyCode, null);
+        }
+        keyMapping.put(keyCode, factory);
+        invertKeyMapping.put(factory, keyCode);
     }
 
-    public Class<? extends Event> getMappingEvent(KeyEvent event) {
-        return keyMapping.getOrDefault(event.getCode(), Event.class);
+    /**
+     * 将鼠标按钮事件映射到事件工厂
+     * @param event 鼠标事件
+     * @return 其对应的事件工厂
+     */
+    public ControlEventFactory getMappingEvent(MouseEvent event) {
+        return mouseMapping.getOrDefault(event.getButton(), null);
+    }
+
+    /**
+     * 将键盘按钮事件映射到事件工厂
+     * @param event 键盘事件
+     * @return 其对应的事件工厂
+     */
+    public ControlEventFactory getMappingEvent(KeyEvent event) {
+        return keyMapping.getOrDefault(event.getCode(), null);
     }
 }
