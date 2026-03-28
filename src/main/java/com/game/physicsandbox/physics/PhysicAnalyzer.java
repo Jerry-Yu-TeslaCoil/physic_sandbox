7a099f8433b56b39852e1510b4e693b1349e66bd
Jerry-Yu-TeslaCoil <jerry.050222@outlook.com>
2026-03-28 12:01:23 +0800

    feat: 为PhysicAnalyzer增加组件更新方法

--- src/main/java/com/game/physicsandbox/physics/PhysicAnalyzer.java
+++ src/main/java/com/game/physicsandbox/physics/PhysicAnalyzer.java
@@ -1,6 +1,26 @@
 package com.game.physicsandbox.physics;
 
 import com.game.physicsandbox.mechanism.ComponentExecutor;
+import com.game.physicsandbox.object.Component;
 
+/**
+ * 物理分析器。物体力学组件在此更新。
+ * 本质上是一个力学组件执行器。
+ *
+ * @author Jerry-Yu-TeslaCoil
+ * @version 1.0
+ */
 public class PhysicAnalyzer extends ComponentExecutor {
+
+    /**
+     * 更新力学组件。
+     * @param currentTime 当前时间纳秒
+     * @param deltaTime 时间增量纳秒
+     */
+    public void update(long currentTime, long deltaTime) {
+        for (Component component : components) {
+            component.update(currentTime, deltaTime);
+        }
+    }
+
 }
